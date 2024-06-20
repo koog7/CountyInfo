bf01a937181a08d04c9201e31cd1414a9c88fa7d
koog7 <k.ognev709@mail.com>
2024-06-20 22:17:21 +0600

    Добавлена функция отображения названий граничащих стран

--- src/components/CountryInfo.tsx
+++ src/components/CountryInfo.tsx
@@ -17,6 +17,7 @@ interface CountryData {
 const CountryInfo: React.FC<Props> = ({name , alpha3Code}) => {
 
     const [countryData, setCountryData] = useState<CountryData>();
+    const [borderCountries , setBorderCountries] = useState<string[]>([])
     const url = `https://restcountries.com/v2/alpha/${alpha3Code}`
 
     useEffect(() => {
@@ -32,7 +33,25 @@ const CountryInfo: React.FC<Props> = ({name , alpha3Code}) => {
 
     useEffect(() => {
         console.log(countryData);
+        setBorderCountries([]);
+        if (countryData && countryData.borders) {
+            countryData.borders.map(async (code) => {
+                try {
+                    const response = await fetch(`https://restcountries.com/v2/alpha/${code}`);
+                    if (response.ok) {
+                        const item = await response.json();
+                        setBorderCountries(prevState => [...prevState, item.name]);
+                        console.log(item.name);
+                    }
+                }catch (error) {
+                    console.error('Error:', error);
+                }
+            });
+        }else {
+            setBorderCountries(['Границ нет']);
+        }
     }, [countryData]);
+
     return (
         <div>
             <p>{name}</p>
@@ -41,8 +60,12 @@ const CountryInfo: React.FC<Props> = ({name , alpha3Code}) => {
                 <>
                     <p>Capital: {countryData.capital}</p>
                     <p>Population: {countryData.population}</p>
-                    <img src={countryData.flags.svg} alt={`${countryData.name} flag`}/>
-                    <p>Borders: {countryData.borders.join(' ')}</p>
+                    <img width={'300px'} src={countryData.flags.svg} alt={`${countryData.name} flag`}/>
+                    <ul>
+                        {borderCountries.map((country, index) => (
+                            <li key={index}>{country}</li>
+                        ))}
+                    </ul>Добавлена функция отображения названий граничащих стран
                 </>
             )}
         </div>
